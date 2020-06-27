2c8a8aec0ce25602df364e1e89db107cd854aa4d
Ruchithaakula <67132687+Ruchithaakula@users.noreply.github.com>
2020-06-27 19:58:00 +0530

    Add files via upload

---
+++ Assignment9_empty or not.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+def Enquiry(lis1): 
+    if len(lis1) == 0: 
+        return 0
+    else: 
+        return 1
+          
+ 
+ lis1 = [1,3,5] 
+if Enquiry(lis1): 
+    print ("The list is not empty") 
+else: 
+    print("Empty List") 
+
+
+# In[ ]:
+
+
+
+
---
+++ Assignment9_muliply items of list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+def multiply_list(items):  
+    tot = items[0]  
+    for x in items:  
+        tot *= x  
+    return tot  
+print(multiply_list([1,2,-8]))  
+
+
+# In[ ]:
+
+
+
+
